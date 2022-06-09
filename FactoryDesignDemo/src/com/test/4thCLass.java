3815936aaf6126987dd39d1d1cdc7c4bff77e730
raja1 <rajeshveerpro@gmail.com>
2022-06-09 13:44:43 +0530

    New File added

---
+++ FactoryDesignDemo/src/com/test/4thCLass.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class 4thClass implements Booking {
+
+	@Override
+	public String getACClass() {
+		return "First class- AC- Seat availability:8";		
+	}
+	
+	// test code added.. 
+	// return "First class- AC- Seat availability:8";		
+
+}
